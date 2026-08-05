02af005b77948b0f27183f9379fe815b18186efd
colbysparks <colby.sparks@zaber.com>
2026-08-04 18:23:22 -0700

    Sync once again with main

--- examples/util_matlab_compiler/src/main.m
+++ examples/util_matlab_compiler/src/main.m
@@ -1,4 +1,4 @@
-function standalone_app()
+function main()
     % Simple example of using the Zaber Motion Library to move a device.
     import zaber.motion.Units;
     import zaber.motion.ascii.Connection;
